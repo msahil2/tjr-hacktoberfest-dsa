adc60b5c072c1d0e1d391bae0074fcf2bd8d1032
msahil2 <161047563+msahil2@users.noreply.github.com>
2025-10-01 15:30:53 +0530

    Update 0013-roman-to-integer.java

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,5 +1,22 @@
 class Solution {
+    public int help(char a){
+        if(a=='M')return 1000;
+        else if(a=='D')return 500;
+        else if(a=='C')return 100;
+        else if(a=='L')return 50;
+        else if(a=='X')return 10;
+        else if(a=='V')return 5;
+        else return 1;
+    }
     public int romanToInt(String s) {
+        int sum=help(s.charAt(s.length()-1));
+        for(int i=s.length()-1;i>0;i--){
+            char ch=s.charAt(i);
+            char ch1=s.charAt(i-1);
+            if(help(ch1)<help(ch))sum-=help(ch1);
+            else sum+=help(ch1);
+        }
+        return sum;
         
     }
 }
